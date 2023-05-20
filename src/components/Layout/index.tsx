a06d4864d93648ed255405035e8abf542023665c
Andrey <somanfrom58@gmail.com>
2023-05-20 20:33:22 +0300

    Add auto-redirect authorized or not user

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,19 +1,18 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import { useAppSelector } from "../../store";
-import { selectAuth } from "../../store/auth/selectors";
+import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
-import Sider from "antd/es/layout/Sider";
 
 export const LayoutPage = () => {
-    const navigate = useNavigate()
-    const { isLoggedIn, user } = useAppSelector(selectAuth)
-    return <>
-        <Layout style={{ minHeight: '100vh' }}>
-            <Layout.Header>header</Layout.Header>
-            <Layout>
-                <Layout.Content style={{padding: '18px 36px'}}><Outlet /></Layout.Content>
-            </Layout>
-            <Layout.Footer>footer</Layout.Footer>
+  return (
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Layout.Header>header</Layout.Header>
+        <Layout>
+          <Layout.Content style={{ padding: "18px 36px" }}>
+            <Outlet />
+          </Layout.Content>
         </Layout>
+        <Layout.Footer>footer</Layout.Footer>
+      </Layout>
     </>
-}
+  );
+};
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,59 +1,56 @@
-import { useCallback, useEffect } from 'react';
-import { loginAsync } from '../store/auth/api';
-import { useAppDispatch, useAppSelector } from '../store';
-import { selectAuth } from '../store/auth/selectors';
-import { useNavigate } from 'react-router-dom';
-import { Button, Col, Form, Row, Input, Checkbox } from 'antd';
+import { useCallback } from "react";
+import { loginAsync } from "../store/auth/api";
+import { useAppDispatch } from "../store";
+import { Button, Form, Input, Checkbox } from "antd";
 
 export const Login = () => {
-    const dispatch = useAppDispatch()
-    const navigate = useNavigate()
-    const user = useAppSelector(selectAuth)
+  const dispatch = useAppDispatch();
 
-    const onSubmit = useCallback(async ({ username, password }: { username: string, password: string }) => {
-        await dispatch(loginAsync({ password, username: username }))
-    }, [dispatch]);
+  const onSubmit = useCallback(
+    async ({ username, password }: { username: string; password: string }) => {
+      await dispatch(loginAsync({ password, username: username }));
+    },
+    [dispatch]
+  );
 
-    useEffect(() => {
-        if (user?.isLoggedIn) {
-            navigate('/profile')
-        }
-    }, [navigate, user])
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600, margin: "auto" }}
+      initialValues={{ remember: true }}
+      onFinish={onSubmit}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
 
-    return (
-        <Form
-            name="basic"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600, margin: 'auto' }}
-            initialValues={{ remember: true }}
-            onFinish={onSubmit}
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
-            >
-                <Input />
-            </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
-            >
-                <Input.Password />
-            </Form.Item>
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
 
-            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-                <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    );
-}
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
